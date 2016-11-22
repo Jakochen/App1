263a9eb167ad8a878fa65f97a6892f8811d7d655
jakochen <bcsddaroka@gmail.com>
2016-11-22 14:10:38 +0800

    新增 mobi app client

---
+++ App1/App1.Droid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net;
+using System.Net.Sockets;
+
+namespace App1.Droid
+{
+    [Activity(Label = "App1", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        //System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            //clientSocket.Connect("172.20.159.13", 8807);
+            //NetworkStream serverStream = clientSocket.GetStream();
+            //byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox2.Text + "$");
+            //serverStream.Write(outStream, 0, outStream.Length);
+            //serverStream.Flush();
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Sockets.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnRESTfulClicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                var address = "172.20.159.13";
+                var port = 8807;
+                var r = new Random();
+
+                var client = new TcpSocketClient();
+                await client.ConnectAsync(address, port);
+
+                // we're connected!
+                for (int i = 0; i < 5; i++)
+                {
+                    // write to the 'WriteStream' property of the socket client to send data
+                    var nextByte = (byte)r.Next(0, 254);
+                    client.WriteStream.WriteByte(nextByte);
+                    await client.WriteStream.FlushAsync();
+
+                    // wait a little before sending the next bit of data
+                    await Task.Delay(TimeSpan.FromMilliseconds(500));
+                }
+
+                await client.DisconnectAsync();
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("INFO",ex.ToString(),"");
+            }
+        }
+    }
+}
